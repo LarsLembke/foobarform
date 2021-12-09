3d2c7124ef1a8bf4187963d90d147d841f075d0c
larslembke <lars46i6@stud.kea.dk>
2021-12-09 13:35:16 +0100

    yghjklæø

--- src/components/Cardnumber.js
+++ src/components/Cardnumber.js
@@ -1,13 +1,33 @@
 
 
 export default function Cardnumber(props) {
+
+  function focusfun(e) {
+    console.log(e.target.value.length)
+
+
+
+    if (e.target.value.length == e.target.getAttribute("maxlength")) {
+
+        console.log(e.target.nextElementSibling);
+
+        e.target.nextElementSibling.focus();
+
+        
+    }
+
+
+}
+
+
+  
   return (
     <>
       <p>Cardnumber</p>
-      <input type="number" pattern="[0-9]*" inputmode="numeric"/>
+      <input type="text" pattern="[0-9]*" inputmode="numeric" maxLength="16" onInput={focusfun}/>
       <div>
         <p>Expiration date</p>
-        <input type="number"/>
+        <input type="text" pattern="[0-9]*" inputmode="numeric"/>
       </div>
     </>
   );
